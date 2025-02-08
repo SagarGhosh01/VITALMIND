bda32dd1aaeabe3dda4e9d1d3b361b36d30ad5e2
Sagar Ghosh <sagarghosh571@gmail.com>
2025-02-09 04:26:44 +0530

    Add files via upload

---
+++ websocket-chat-master/websocket-chat-master/server/server.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const socketIo = require('socket.io');
+
+const server = http.createServer();
+const io = socketIo(server);
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+    
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
