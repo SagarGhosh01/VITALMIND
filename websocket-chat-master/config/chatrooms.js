7507c5337905819ed5201fbf9b3e6b35047ecb28
Sagar Ghosh <sagarghosh571@gmail.com>
2025-02-09 04:16:15 +0530

    Add files via upload

---
+++ websocket-chat-master/config/chatrooms.js
@@ -0,0 +1,19 @@
+module.exports = [
+  {
+    name: 'Hostel',
+    image: 'chatrooms/Hostel.jpg'
+  },
+  {
+    name: 'Room',
+    image: 'chatrooms/room.jpg'
+  },
+  {
+    name: 'Clinic',
+    image: 'chatrooms/Clinic.jpeg'
+  },
+  {
+    name: 'Classroom',
+    image: 'chatrooms/Classroom.jpg'
+  }
+]
+
---
+++ websocket-chat-master/config/users.js
@@ -0,0 +1,27 @@
+module.exports = [
+  {
+    name: 'Divyansh',
+    lastName: 'Chandrakar',
+    statusText: 'I like Cats',
+    image: 'users/cat 1.jpg'
+  },
+  {
+    name: 'Rajveer',
+    lastName: 'Panwar',
+    statusText: 'I like more Cats.',
+    image: 'users/cat 2.jpg'
+  },
+  {
+    name: 'Sagar',
+    lastName: 'Ghosh',
+    statusText: 'I like more more cats',
+    image: 'users/fish.jpg'
+  },
+  {
+    name: 'Sanjana',
+    lastName: 'Bhanjare',
+    statusText: 'I like more more more cats',
+    image: 'users/cat sanjana.jpg'
+  }
+]
+
